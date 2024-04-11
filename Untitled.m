dc42c6f641ea5cf3c04f2b43f1723616464d3a78
hextre <hextre@users.noreply.github.com>
2024-04-11 20:13:17 +0530

    Add files via upload

---
+++ Untitled.m
@@ -0,0 +1,11 @@
+clc
+clear all
+%% Use this Code to generate dataset for prime Numbers less than N = 1000
+data=primes(1000);
+
+r=data;
+theta=data;
+
+% Polar Coordinate Plots
+polarscatter(theta,r);
+%%
